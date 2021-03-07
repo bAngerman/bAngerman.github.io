c6f00a94d97af93bb270e97bee1d0449fb57079d
bAngerman <brendan.angerman@gmail.com>
2021-03-06 17:29:55 -0700

    init

---
+++ _posts/2021-03-06-hello-world.md
@@ -0,0 +1,10 @@
+---
+title: Hello World
+tags: [PHP]
+style: fill
+color: warning
+description: A test post.
+# external_url: 
+---
+
+# Test Post
---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: [
+      './_layouts/**/*.html',
+      './_includes/**/*.html',
+    ],
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
